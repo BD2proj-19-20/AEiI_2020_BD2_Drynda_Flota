4fb7fb02fb7ae4a1314bacda935a943d79a1588d
Piotr Uhl <piotruhl@gmail.com>
2020-05-12 14:59:58 +0200

    Rozwój projektu bazy danych

---
+++ FirmaTransportowa/Database/Contractors.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Contractors]
+(
+	[id] INT NOT NULL PRIMARY KEY, 
+    [name] VARCHAR(63) NOT NULL
+)
--- FirmaTransportowa/Database/people.sql
+++ FirmaTransportowa/Database/people.sql
@@ -2,5 +2,9 @@
 (
 	[id] INT NOT NULL PRIMARY KEY, 
     [firstName] NVARCHAR(63) NOT NULL, 
-    [lastName] NVARCHAR(63) NOT NULL
+    [lastName] NVARCHAR(63) NOT NULL, 
+    [employmentData] SMALLDATETIME NOT NULL, 
+    [layoffDate] SMALLDATETIME NULL, 
+    [systemLogin] VARCHAR(63) NULL, 
+    [passwordHash] BINARY(256) NULL
 )
