9537f9ccce7bc94d4b617abc457d714f8161ddec
Mariusz Drynda <mariusz1998@wp.pl>
2020-06-14 14:52:27 +0200

    Widok Pracowników + poczatek ich dodania

--- FirmaTransportowa/FirmaTransportowa/Views/Pracownicy.xaml.cs
+++ FirmaTransportowa/FirmaTransportowa/Views/Pracownicy.xaml.cs
@@ -13,23 +13,55 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FirmaTransportowa.Model;
+using FirmaTransportowa.ViewModels;
 
 namespace FirmaTransportowa.Views
 {
     /// <summary>
     /// Interaction logic for Pracownicy.xaml
     /// </summary>
+    /// 
+    public class WorkersList
+    {
+        public string Person { get; set; }
+    }
+
+
     public partial class Pracownicy : UserControl
     {
+
+        List<WorkersList> items = new List<WorkersList>();
+
         public Pracownicy()
         {
             InitializeComponent();
+            var db = new AEiI_2020_BD2_Drynda_FlotaEntities();
+            var people = db.People;
+            foreach (var person in people)
+            {
+                items.Add(new WorkersList { Person = person.firstName + " " + person.lastName });
+            }
+            
+            workersList.ItemsSource = items;
+
+            //CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(carList.ItemsSource);
+            //  view.Filter += UserFilter;
+
+
         }
+        private void Dodaj_Pracownika(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Window glowneOkno = System.Windows.Application.Current.MainWindow;
+            glowneOkno.DataContext = new DodajPracownikModel();
+        }
+
 
         private void WorkerStatistics_Click(object sender, RoutedEventArgs e)
         {
-                System.Windows.Window mainWindow = System.Windows.Application.Current.MainWindow;
-                mainWindow.DataContext = new StatystykiPracownikaModel();
+            System.Windows.Window mainWindow = System.Windows.Application.Current.MainWindow;
+            mainWindow.DataContext = new StatystykiPracownikaModel();
         }
+
     }
 }
