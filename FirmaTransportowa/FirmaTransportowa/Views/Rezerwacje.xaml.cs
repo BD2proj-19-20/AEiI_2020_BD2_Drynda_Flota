3ec07abc1e9d997b572f10651be384be6cd88214
Mariusz Drynda <mariusz1998@wp.pl>
2020-07-28 18:16:44 +0200

    Początek rezerwacji

--- FirmaTransportowa/FirmaTransportowa/Views/Rezerwacje.xaml.cs
+++ FirmaTransportowa/FirmaTransportowa/Views/Rezerwacje.xaml.cs
@@ -13,31 +13,121 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.ComponentModel;
+using FirmaTransportowa.Model;
+using FirmaTransportowa.ViewModels;
 
 namespace FirmaTransportowa.Views
 {
     /// <summary>
     /// Interaction logic for Rezerwacje.xaml
     /// </summary>
+    /// 
+    public class ReservationList
+    {
+        public int ReservationId { get; set; }
+        public string Person { get; set; }
+        public string ReservationStart { get; set; }
+        public string ReservationEnd { get; set; }
+        public string ReservationDate { get; set; }
+        public string Vehicle { get; set; }
+
+
+    }
     public partial class Rezerwacje : UserControl
     {
         private GridViewColumnHeader listViewSortCol = null;
         private SortAdorner listViewSortAdorner = null;
+
+        List<ListViewItem> items = new List<ListViewItem>();
+
+        public void UpdateView()
+        {
+            ListaRezerwacji();
+        }
         public Rezerwacje()
         {
             InitializeComponent();
-            List<Reservations> items = new List<Reservations>();
-            items.Add(new Reservations() { Id = 101, Name="Jan Kowalski", Date = "14 03 2020", Car = "Skoda Octavia", Keeper="Jan Nowak" });
-            items.Add(new Reservations() { Id = 102, Name = "Jan Kowalski", Date = "14 03 2019", Car = "Ford Focus", Keeper = "Jan Nowak" });
-            items.Add(new Reservations() { Id = 103, Name = "Jan Kowalski", Date = "12 05 2019", Car = "Audi A8", Keeper = "Jan Nowak" });
-            items.Add(new Reservations() { Id = 104, Name = "Jan Kowalski", Date = "25 03 2019", Car = "Volkswagen Passat", Keeper = "Jan Nowak" });
-            items.Add(new Reservations() { Id = 105, Name = "Jan Kowalski", Date = "30 01 2019", Car = "Ford Mondeo", Keeper = "Jan Nowak" });
-            items.Add(new Reservations() { Id = 106, Name = "Jan Kowalski", Date = "22 12 2019", Car = "Ford Focus", Keeper = "Jan Nowak" });
-            ListViewReservations.ItemsSource = items;
+            UpdateView();
 
             //CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(ListViewMyReservations.ItemsSource);
             // view.SortDescriptions.Add(new SortDescription("Id", ListSortDirection.Ascending));
         }
+
+        public void ListaRezerwacji()
+        {
+            var db = new AEiI_2020_BD2_Drynda_FlotaEntities();
+            var people = db.People;
+            var reservations = db.Reservations;
+            var cars = db.Cars;
+
+            foreach (var reserv in reservations)
+            {
+                ListViewItem OneItem = new ListViewItem();
+                var date = "";
+                //checkbox zrob
+                var opiekun = "";
+                var vehicle = "";
+
+                foreach (var person in people)
+                {
+                    if (reserv.personId == person.id)
+                        opiekun = person.lastName + " " + person.firstName;
+
+                }
+                foreach (var car in cars)
+                {
+                    if (car.id == reserv.carId)
+                        vehicle = car.CarModel.make + "/" + car.CarModel.model + "/" + car.Registration + "\n";
+
+
+                }
+                if (reserv.ended == true)
+                {
+
+                    OneItem.Background = Brushes.OrangeRed;
+                    string dateTime = reserv.lendDate.ToString();
+                    date = dateTime.Substring(0, 10);
+
+                    OneItem.Content = new ReservationList
+                    {
+                        ReservationId = reserv.id + 1,
+                        Person = opiekun,
+                        ReservationStart = date,
+                        ReservationEnd = reserv.returnDate.ToString().Substring(0, 10),
+                        ReservationDate = reserv.reservationDate.ToString().Substring(0, 10),
+                        Vehicle = vehicle
+                    };
+                    items.Add(OneItem);
+                }
+                else
+                {
+                    string dateTime = reserv.lendDate.ToString();
+                    date = dateTime.Substring(0, 10);
+
+                    OneItem.Content = new ReservationList
+                    {
+                        ReservationId = reserv.id + 1,
+                        Person = opiekun,
+                        ReservationStart = date,
+                        ReservationEnd = reserv.returnDate.ToString().Substring(0, 10),
+                        ReservationDate = reserv.reservationDate.ToString().Substring(0, 10),
+                        Vehicle = vehicle
+                    };
+                    items.Add(OneItem);
+                }
+
+
+            }
+            ListViewReservations.ItemsSource = items;
+        }
+
+
+        private void Dodaj_Rezerwacje(object sender, RoutedEventArgs e)
+        {
+          //  System.Windows.Window glowneOkno = System.Windows.Application.Current.MainWindow;
+          //  glowneOkno.DataContext = new DodajRezerwacjeModel();
+        }
+
         private void GridViewColumnHeader_Click(object sender, RoutedEventArgs e)
         {
             GridViewColumnHeader column = (sender as GridViewColumnHeader);
@@ -57,13 +147,5 @@ namespace FirmaTransportowa.Views
             AdornerLayer.GetAdornerLayer(listViewSortCol).Add(listViewSortAdorner);
             ListViewReservations.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
         }
-    }
-    public class Reservations
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Date { get; set; }
-        public string Car { get; set; }
-        public string Keeper { get; set; }
     }
 }
