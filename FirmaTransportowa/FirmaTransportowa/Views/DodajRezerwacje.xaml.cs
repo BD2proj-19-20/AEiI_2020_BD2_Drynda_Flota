270b91cb2b946747167a4cb59888b8734e3b3269
Mariusz Drynda <mariusz1998@wp.pl>
2020-07-28 19:47:02 +0200

    Dodaj rezerwacje - ekran wybierania opiekuna i pojazdu

--- FirmaTransportowa/FirmaTransportowa/Views/DodajRezerwacje.xaml.cs
+++ FirmaTransportowa/FirmaTransportowa/Views/DodajRezerwacje.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FirmaTransportowa.Model;
+using FirmaTransportowa.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,82 @@ namespace FirmaTransportowa.Views
         public DodajRezerwacje()
         {
             InitializeComponent();
+            ReservationDate.Text = DateTime.Today.ToString("dd.MM.yyyy");
+            ReservationDate.IsReadOnly = true;
+            var db = new AEiI_2020_BD2_Drynda_FlotaEntities();
+
+            var people = db.People;
+
+            foreach (var human in people)
+            {
+                Pracownicy.Items.Add(human.firstName + " " + human.lastName);
+            }
+          
+
+            var cars = db.Cars;
+            foreach (var car in cars)
+            {
+
+                PojazdID.Items.Add(car.id);
+
+            }
+            // var aaa = Pojazdy.Text;
+            PojazdID.SelectedIndex = 0;
+            Dane_Pojzadu();
+
+        }
+
+
+        private void Dane_Pojzadu()
+        {
+            var db = new AEiI_2020_BD2_Drynda_FlotaEntities();
+            var cars = db.Cars;
+            foreach (var car in cars)
+            {
+                if ((car.id).ToString() == PojazdID.Text)
+                {
+                    Rejestracja.Text = car.Registration;
+                    PojemnoscSilnika.Text = car.engineCapacity.ToString();
+
+                    var carmodel = db.CarModels;
+                    foreach (var carModel in carmodel)
+                    {
+                        if (car.modelId == carModel.id)
+                        {
+                            Marka.Text = car.CarModel.make;
+                            Model.Text = car.CarModel.model;
+                        }
+                    }
+                    var carDes = db.CarDestinations;
+                    foreach(var cardes in carDes)
+                    {
+                        if (car.destinationId == cardes.id)
+                            Zastosowanie.Text = cardes.name;      
+                    }
+                }
+            }
+            Rejestracja.IsReadOnly = true;
+            PojemnoscSilnika.IsReadOnly = true;
+            Marka.IsReadOnly = true;
+            Model.IsReadOnly = true;
+            Zastosowanie.IsReadOnly = true;
+
+        }
+         private void Pojazd_Change(object sender, RoutedEventArgs e)
+        {
+            Dane_Pojzadu();
+        }
+
+        private void Dodaj_Rezerwacje(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Cofnij(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Window glowneOkno = System.Windows.Application.Current.MainWindow;
+            glowneOkno.DataContext = new DodajRezerwacjeModel();
+
         }
     }
 }
