188e5d3d2c11f146443453764c607df32b0d4a85
DanielMagik <danik98@onet.pl>
2020-09-12 21:38:00 +0200

    Dodano checkboxa dla rezerwacji aktualne/nieaktualne

--- FirmaTransportowa/FirmaTransportowa/Views/RezerwacjePojazdu.xaml.cs
+++ FirmaTransportowa/FirmaTransportowa/Views/RezerwacjePojazdu.xaml.cs
@@ -33,29 +33,45 @@ namespace FirmaTransportowa.Views
         private int userPermission;
         public RezerwacjePojazdu(int permission, Car car)
         {
-            
             InitializeComponent();
             car1 = car;
             userPermission = permission;
             Title.Content = "Rezerwacje pojazdu: " + car.CarModel.make + " " + car.CarModel.model +" "+car.Registration;
-            for (int i = 0; i < car.Reservations.Count; i++)
-            {
-                this.ListViewReservations.Items.Add(new Reservation
-                {
-                    Person = car.Reservations.ElementAt(i).Person.firstName + " " + car.Reservations.ElementAt(i).Person.lastName,
-                    ReservationStart = car.Reservations.ElementAt(i).lendDate.ToString(),
-                    ReservationEnd = car.Reservations.ElementAt(i).reservationDate.ToString(),
-                    ReservationDate = car.Reservations.ElementAt(i).returnDate.ToString()
-                }) ;
-            }
-
-
+            loadTable();
         }
 
         private void cofnij(object sender, RoutedEventArgs e)
         {
             System.Windows.Window glowneOkno = System.Windows.Application.Current.MainWindow;
             glowneOkno.DataContext = new StatystykiPojazdu(car1, userPermission);
+        }
+
+        private void changeAktualne(object sender, RoutedEventArgs e)
+        {
+            if (AktualneBox.IsChecked == true)
+                Title.Content = "Aktualne rezerwacje pojazdu: " + car1.CarModel.make + " " + car1.CarModel.model + " " + car1.Registration;
+            else
+                Title.Content = "Zakończone rezerwacje pojazdu: " + car1.CarModel.make + " " + car1.CarModel.model + " " + car1.Registration;
+
+            loadTable();
+        }
+        void loadTable()
+        {
+            this.ListViewReservations.Items.Clear();
+            for (int i = 0; i < car1.Reservations.Count; i++)
+            {
+               if ((AktualneBox.IsChecked == true && DateTime.Compare((DateTime)car1.Reservations.ElementAt(i).returnDate, DateTime.Now) <0)
+                    || (AktualneBox.IsChecked == false))
+                {
+                    this.ListViewReservations.Items.Add(new Reservation
+                    {
+                        Person = car1.Reservations.ElementAt(i).Person.firstName + " " + car1.Reservations.ElementAt(i).Person.lastName,
+                        ReservationStart = car1.Reservations.ElementAt(i).lendDate.ToString(),
+                        ReservationEnd = car1.Reservations.ElementAt(i).reservationDate.ToString(),
+                        ReservationDate = car1.Reservations.ElementAt(i).returnDate.ToString()
+                    }) ;
+                }
+            }
         }
     }
 }
