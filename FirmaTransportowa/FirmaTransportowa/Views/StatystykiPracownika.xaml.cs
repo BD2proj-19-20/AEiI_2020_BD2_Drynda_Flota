6daad33a28134ba66d337088c5834a94e1dca718
Mariusz Drynda <mariusz1998@wp.pl>
2020-09-20 16:35:04 +0200

    staty danego pracownika jest uporanie się z błędem z nullem

--- FirmaTransportowa/FirmaTransportowa/Views/StatystykiPracownika.xaml.cs
+++ FirmaTransportowa/FirmaTransportowa/Views/StatystykiPracownika.xaml.cs
@@ -26,7 +26,7 @@ namespace FirmaTransportowa.Views
 
                 KierownikStartPanel.Visibility = Visibility.Hidden;
                 KierownikEndPanel.Visibility = Visibility.Hidden;
-               zmienLoginButton.Visibility = Visibility.Hidden;
+                zmienLoginButton.Visibility = Visibility.Hidden;
                 zmienpassowrdButton.Visibility = Visibility.Hidden;
                 zmienKierownikaButton.Visibility = Visibility.Hidden;
                 OpiekunPanel.Visibility = Visibility.Hidden; //nie może byc opiekunem zwolniony pracownik
@@ -43,203 +43,192 @@ namespace FirmaTransportowa.Views
 
             var db = new AEiI_2020_BD2_Drynda_FlotaEntities();
 
-            var query = from person in db.People where person.id == changePerson.id
-                        join peoplePermission in db.PeoplesPermissions on person.id equals peoplePermission.personId into permissionTable
+            var worker = (from peoplee in db.People
+                          where peoplee.id == changePerson.id
+                          select peoplee).FirstOrDefault();
 
-                        from permissionPeople in permissionTable.DefaultIfEmpty()
-                        select new
-                        {
-                            Id = person.id,
-                            LastName = person.lastName,
-                            FirstName = person.firstName,
-                            Login = person.systemLogin,
-                            EmploymentData = person.employmentData,
-                            LayoffDate = person.layoffDate,
-                            PermissionName = permissionPeople.Permission.name,
-                            PermissionGrant = permissionPeople.grantDate,// == null ? DateTime.MinValue : permissionPeople.grantDate,
-                            RevokeDate = permissionPeople.revokeDate// == null ? DateTime.MinValue : permissionPeople.revokeDate,
+            var workerPermission = (from personPermission in db.PeoplesPermissions
+                                    where personPermission.personId == changePerson.id
+                                    select personPermission).FirstOrDefault();
 
-                        };
 
-            foreach (var person in query)
+
+            var query2 = from supervisor in db.CarSupervisors
+                         where worker.id == supervisor.personId
+                         join car in db.Cars on supervisor.carId equals car.id
+                         select new
+                         {
+                             BeginDate = supervisor.beginDate == null ? DateTime.MinValue : supervisor.beginDate,
+                             EndDate = supervisor.endDate == null ? DateTime.MinValue : supervisor.endDate,
+                             SaleDate = car.saleDate == null ? DateTime.MinValue : car.saleDate,
+                             CarMake = car.CarModel.make,
+                             CarModel = car.CarModel.model,
+                             CarRegistration = car.Registration,
+                         };
+
+
+            var query3 = from lends2 in db.Lends
+                         where lends2.personId == worker.id
+                         select new
+                         {
+                             LendDate = lends2.lendDate == null ? DateTime.MinValue : lends2.lendDate,
+                             EngineCar = lends2.Car.engineCapacity, // == null ? 0 : lends2.Car.engineCapacity,
+                             ReservationEnd = lends2.Reservation.ended,
+                             ReturnDate = lends2.returnDate == null ? DateTime.MinValue : lends2.returnDate,
+                             Private = lends2.@private,
+                             StartOdometer = lends2.startOdometer,
+                             StartFuel = lends2.startFuel,
+                             EndOdometer = lends2.endOdometer,
+                             EndFuel = lends2.endFuel,
+                             PlannedReturnDate = lends2.plannedReturnDate,
+                             LendedCar = lends2.Car
+                         };
+
+            ImieNazwisko.Text = worker.firstName + " " + worker.lastName;
+            DataZatrudnienia.Text = worker.employmentData.ToString().Substring(0, 10);
+            if (worker.layoffDate != null)
+                DataZwolnienia.Text = worker.layoffDate.ToString().Substring(0, 10);
+            Login.Text = worker.systemLogin;
+
+
+            var carSupervisior = db.CarSupervisors;
+
+            var lends = db.Lends;
+            var cars = db.Cars;
+            string textOpiekun = "";
+            string bylyOpiekun = "";
+
+            foreach (var personSup in query2)
+
             {
-                //Tutaj beda wszystkie pojazdy, jakich pracownik byl opiekunem
-                var query2 = from supervisor in db.CarSupervisors
-                             where person.Id == supervisor.personId
-                             join car in db.Cars on supervisor.carId equals car.id
-                             select new
-                             {
-                                 BeginDate = supervisor.beginDate == null ? DateTime.MinValue : supervisor.beginDate,
-                                 EndDate = supervisor.endDate == null ? DateTime.MinValue : supervisor.endDate,
-                                 SaleDate = car.saleDate == null ? DateTime.MinValue : car.saleDate,
-                                 CarMake = car.CarModel.make,
-                                 CarModel = car.CarModel.model,
-                                 CarRegistration = car.Registration,
-                             };
+                if ((personSup.EndDate > DateTime.Today || personSup.EndDate == null || personSup.EndDate == DateTime.MinValue) &&
+                        (personSup.SaleDate > DateTime.Today || personSup.SaleDate == null || personSup.SaleDate == DateTime.MinValue))
+                    textOpiekun += personSup.CarMake + "/" + personSup.CarModel + "/" + personSup.CarRegistration + "\n";
+                else
+                    bylyOpiekun += personSup.CarMake + "/" + personSup.CarModel + "/" + personSup.CarRegistration + "\n";
+
+            }
+
+            if (!(changePerson.layoffDate <= DateTime.Now))
+                Opiekun.Text = textOpiekun;
+            BylyOpiekun.Text = bylyOpiekun;
+
+            Kierownik.Text = "Nie";
+
+            if (workerPermission!=null && workerPermission.Permission.name == "Kierownik" &&
+          workerPermission.grantDate <= DateTime.Now.Date && (workerPermission.revokeDate > DateTime.Now || workerPermission.revokeDate == null))
+            {
+                KierownikEndPanel.Visibility = Visibility.Visible;
+                KierownikStartPanel.Visibility = Visibility.Visible;
+                Kierownik.Text = "Tak";
+                KierownikDateStart.Text = workerPermission.grantDate.ToString().Substring(0, 10);
+                if (workerPermission.revokeDate != null)
+                    KierownikDateEnd.Text = workerPermission.revokeDate.ToString().Substring(0, 10);
+
+            }
+            else if (workerPermission != null && workerPermission.Permission.name == "Kierownik" && workerPermission.grantDate > DateTime.Now.Date)
+            {
+                KierownikEndPanel.Visibility = Visibility.Visible;
+                KierownikStartPanel.Visibility = Visibility.Visible;
+
+                if (((workerPermission.grantDate - DateTime.Now).Days + 1) == 1)
+                    Kierownik.Text = "Za " + ((workerPermission.grantDate - DateTime.Now.Date).Days + 1) + " dzień";
+                else
+                    Kierownik.Text = "Za " + ((workerPermission.grantDate - DateTime.Now.Date).Days + 1) + " dni";
+                KierownikDateStart.Text = workerPermission.grantDate.ToString().Substring(0, 10);
+                if (workerPermission.revokeDate != null)
+                    KierownikDateEnd.Text = workerPermission.revokeDate.ToString().Substring(0, 10);
+            }
+            else
+            {
+                KierownikEndPanel.Visibility = Visibility.Hidden;
+                KierownikStartPanel.Visibility = Visibility.Hidden;
+            }
+            int zleceniaPrywatne = 0;
+            int przejechaneKm = 0;
+            int zleceniaSluzbowe = 0;
+            int przejechaneKmSluzbowe = 0;
 
 
-                var query3 = from lends2 in db.Lends
-                             where lends2.personId == person.Id
-                             select new
-                             {
-                                 LendDate = lends2.lendDate == null ? DateTime.MinValue : lends2.lendDate,
-                                 EngineCar = lends2.Car.engineCapacity, // == null ? 0 : lends2.Car.engineCapacity,
-                                 ReservationEnd = lends2.Reservation.ended,
-                                 ReturnDate = lends2.returnDate == null ? DateTime.MinValue : lends2.returnDate,
-                                 Private = lends2.@private,
-                                 StartOdometer = lends2.startOdometer,
-                                 StartFuel = lends2.startFuel,
-                                 EndOdometer = lends2.endOdometer,
-                                 EndFuel = lends2.endFuel,
-                                 PlannedReturnDate = lends2.plannedReturnDate,
-                                 LendedCar = lends2.Car
-                             };
+            int dni = 0;
+            int dniSluzbowe = 0;
 
-                ImieNazwisko.Text = person.FirstName + " " + person.LastName;
-                DataZatrudnienia.Text = person.EmploymentData.ToString().Substring(0, 10);
-                if (people.layoffDate != null)
-                    DataZwolnienia.Text = person.LayoffDate.ToString().Substring(0, 10);
-                Login.Text = person.Login;
+            var pojazd = "";
+            var pojazdSluzbowy = "";
 
+            double koszty = 0;
+            double kosztySluzbowe = 0;
 
-                var carSupervisior = db.CarSupervisors;
+            foreach (var personLend in query3)
+            {
 
-                var lends = db.Lends;
-                var cars = db.Cars;
-                string textOpiekun = "";
-                string bylyOpiekun = "";
-
-                foreach (var personSup in query2)
-
+                if (personLend.ReservationEnd == true && personLend.ReturnDate != null) //lend ktore były
                 {
-                    if ((personSup.EndDate > DateTime.Today || personSup.EndDate == null || personSup.EndDate == DateTime.MinValue) &&
-                            (personSup.SaleDate > DateTime.Today || personSup.SaleDate == null || personSup.SaleDate == DateTime.MinValue))
-                        textOpiekun += personSup.CarMake + "/" + personSup.CarModel + "/" + personSup.CarRegistration + "\n";
-                    else
-                        bylyOpiekun += personSup.CarMake + "/" + personSup.CarModel + "/" + personSup.CarRegistration + "\n";
+                    if (personLend.Private == true)
+                    {
+                        zleceniaPrywatne++;
+                        if (personLend.EndOdometer != null)
+                            przejechaneKm += personLend.EndOdometer.Value - personLend.StartOdometer;
+                        if (personLend.ReturnDate > personLend.LendDate)
+                        {
+                            TimeSpan t = (DateTime)personLend.ReturnDate - personLend.LendDate;
+                            dni += (int)t.TotalDays;
+                        }
 
-                }
+                        //jaki to samochód;
 
-                if (!(changePerson.layoffDate <= DateTime.Now))
-                    Opiekun.Text = textOpiekun;
-                BylyOpiekun.Text = bylyOpiekun;
-
-                Kierownik.Text = "Nie";
-
-                if (person.PermissionName == "Kierownik" &&
-               person.PermissionGrant <= DateTime.Now.Date && (person.RevokeDate > DateTime.Now || person.RevokeDate == null))
-                {
-                        KierownikEndPanel.Visibility = Visibility.Visible;
-                        KierownikStartPanel.Visibility = Visibility.Visible;
-                        Kierownik.Text = "Tak";
-                        KierownikDateStart.Text = person.PermissionGrant.ToString().Substring(0, 10);
-                    if (person.RevokeDate != null)
-                        KierownikDateEnd.Text = person.RevokeDate.ToString().Substring(0, 10);
+                        koszty = (przejechaneKm * 4.75) + (0.05 * personLend.EngineCar);
 
                     }
-                else if (person.PermissionName == "Kierownik" && person.PermissionGrant > DateTime.Now.Date)
-                {
-                        KierownikEndPanel.Visibility = Visibility.Visible;
-                        KierownikStartPanel.Visibility = Visibility.Visible;
-
-                    if (((person.PermissionGrant - DateTime.Now).Days + 1) == 1)
-                        Kierownik.Text = "Za " + ((person.PermissionGrant - DateTime.Now.Date).Days + 1) + " dzień";
-                    else
-                            Kierownik.Text = "Za " + ((person.PermissionGrant - DateTime.Now.Date).Days + 1) + " dni";
-                    KierownikDateStart.Text = person.PermissionGrant.ToString().Substring(0, 10);
-                    if (person.RevokeDate != null)
-                        KierownikDateEnd.Text = person.RevokeDate.ToString().Substring(0, 10);
-                }
                     else
                     {
-                        KierownikEndPanel.Visibility = Visibility.Hidden;
-                        KierownikStartPanel.Visibility = Visibility.Hidden;
+                        zleceniaSluzbowe++;
+                        if (personLend.EndOdometer != null)
+                            przejechaneKmSluzbowe += personLend.EndOdometer.Value - personLend.StartOdometer;
+                        if (personLend.ReturnDate > personLend.LendDate)
+                        {
+                            TimeSpan t = (DateTime)personLend.ReturnDate - personLend.LendDate;
+                            dniSluzbowe += (int)t.TotalDays;
+                        }
+
+                        kosztySluzbowe = (przejechaneKmSluzbowe * 4.75) + (0.05 * personLend.EngineCar);
                     }
-                int zleceniaPrywatne = 0;
-                int przejechaneKm = 0;
-                int zleceniaSluzbowe = 0;
-                int przejechaneKmSluzbowe = 0;
+                }
 
-
-                int dni = 0;
-                int dniSluzbowe = 0;
-
-                var pojazd = "";
-                var pojazdSluzbowy = "";
-
-                double koszty = 0;
-                double kosztySluzbowe = 0;
-
-                foreach (var personLend in query3)
+                if (personLend.LendDate < DateTime.Today && personLend.PlannedReturnDate < DateTime.Today &&
+                    personLend.ReturnDate > DateTime.Today) //aktualny pojazd
                 {
 
-                    if (personLend.ReservationEnd == true && personLend.ReturnDate != null) //lend ktore były
+                    foreach (var personSup in query2)
                     {
                         if (personLend.Private == true)
-                        {
-                            zleceniaPrywatne++;
-                            if (personLend.EndOdometer != null)
-                                przejechaneKm += personLend.EndOdometer.Value - personLend.StartOdometer;
-                            if (personLend.ReturnDate > personLend.LendDate)
-                            {
-                                TimeSpan t = (DateTime)personLend.ReturnDate - personLend.LendDate;
-                                dni += (int)t.TotalDays;
-                            }
-
-                            //jaki to samochód;
-
-                            koszty = (przejechaneKm * 4.75) + (0.05 * personLend.EngineCar);
-
-                        }
+                            pojazd += personSup.CarMake + "/" + personSup.CarModel + "/" + personSup.CarRegistration + "\n";
                         else
-                        {
-                            zleceniaSluzbowe++;
-                            if (personLend.EndOdometer != null)
-                                przejechaneKmSluzbowe += personLend.EndOdometer.Value - personLend.StartOdometer;
-                            if (personLend.ReturnDate > personLend.LendDate)
-                            {
-                                TimeSpan t = (DateTime)personLend.ReturnDate - personLend.LendDate;
-                                dniSluzbowe += (int)t.TotalDays;
-                            }
-
-                            kosztySluzbowe = (przejechaneKmSluzbowe * 4.75) + (0.05 * personLend.EngineCar);
-                        }
+                            pojazdSluzbowy += personSup.CarMake + "/" + personSup.CarModel + "/" + personSup.CarRegistration + "\n";
                     }
-
-                    if (personLend.LendDate < DateTime.Today && personLend.PlannedReturnDate < DateTime.Today &&
-                        personLend.ReturnDate > DateTime.Today) //aktualny pojazd
-                    {
-
-                        foreach (var personSup in query2)
-                        {
-                            if (personLend.Private == true)
-                                pojazd += personSup.CarMake + "/" + personSup.CarModel + "/" + personSup.CarRegistration + "\n";
-                            else
-                                pojazdSluzbowy += personSup.CarMake + "/" + personSup.CarModel + "/" + personSup.CarRegistration + "\n";
-                        }
-                    }
-
                 }
 
-                //Koszty???
-                Pojazd.Text = pojazd;
-                PojazdSluzbowe.Text = pojazdSluzbowy;
-
-
-                Dni.Text = dni.ToString() + " dni";
-                DniSluzbowe.Text = dniSluzbowe.ToString() + " dni";
-
-                Zlecenia.Text = zleceniaPrywatne.ToString();
-                Przejechane.Text = przejechaneKm.ToString() + " km";
-
-
-                PrzejechaneSluzobowe.Text = przejechaneKmSluzbowe.ToString() + " km";
-                ZleceniaSluzbowe.Text = zleceniaSluzbowe.ToString();
-
-                Koszty.Text = koszty.ToString() + " PLN";
-                KosztySluzbowe.Text = kosztySluzbowe.ToString() + " PLN";
             }
-        }
+
+            //Koszty???
+            Pojazd.Text = pojazd;
+            PojazdSluzbowe.Text = pojazdSluzbowy;
+
+
+            Dni.Text = dni.ToString() + " dni";
+            DniSluzbowe.Text = dniSluzbowe.ToString() + " dni";
+
+            Zlecenia.Text = zleceniaPrywatne.ToString();
+            Przejechane.Text = przejechaneKm.ToString() + " km";
+
+
+            PrzejechaneSluzobowe.Text = przejechaneKmSluzbowe.ToString() + " km";
+            ZleceniaSluzbowe.Text = zleceniaSluzbowe.ToString();
+
+            Koszty.Text = koszty.ToString() + " PLN";
+            KosztySluzbowe.Text = kosztySluzbowe.ToString() + " PLN";
+
+    }
         private void Cofnij(object sender, RoutedEventArgs e)
         {
 
