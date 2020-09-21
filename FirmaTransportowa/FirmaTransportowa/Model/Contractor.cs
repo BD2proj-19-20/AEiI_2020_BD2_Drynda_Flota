3b4f04db7b5c6594a0b2c0767f0e12dcdf9ad71e
Piotr Uhl <piotruhl@gmail.com>
2020-09-21 20:45:47 +0200

    Update bazy danych: Contractor.endDate jest smalldaterime a nie varchar(10)

--- FirmaTransportowa/FirmaTransportowa/Model/Contractor.cs
+++ FirmaTransportowa/FirmaTransportowa/Model/Contractor.cs
@@ -23,7 +23,7 @@ namespace FirmaTransportowa.Model
         public int id { get; set; }
         public string name { get; set; }
         public System.DateTime startSate { get; set; }
-        public string endDate { get; set; }
+        public Nullable<System.DateTime> endDate { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Activity> Activities { get; set; }
